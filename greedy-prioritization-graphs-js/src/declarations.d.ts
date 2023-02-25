f4472d556be0b6e77af58ce9e67362e73649518f
Liz Bennett <lbennett@confluent.io>
2023-02-25 11:12:17 -0800

    convert to typescript

---
+++ greedy-prioritization-graphs-js/src/declarations.d.ts
@@ -0,0 +1,7 @@
+declare module "*.jpg"
+declare module "*.png"
+declare module "*.svg"
+// We need to tell TypeScript that when we write "import styles from
+// './styles.scss' we mean to load a module (to look for a
+// './styles.scss.d.ts').
+declare module '*.scss';
